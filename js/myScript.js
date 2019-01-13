4cc898dd83cad7567796c362cda3fd616c338a8a
Benoit Semet <benoitsemet1@gmail.com>
2019-01-13 16:47:52 +0100

    Ajout de la galerie

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,52 @@
 jQuery(document).ready(function($) {
-    // Votre code ici avec les appels à la fonction $()
-    $("#content").load("includes/galerie.html");
+    //$("#content").load("includes/galerie.html");
+    $("#content1").load("includes/work.html");
+    $("#content2").load("includes/skills.html");
+
+    $("#content1").hide();
+    $("#content2").hide();
+    $("#content3").hide();
+
+    $('#awork').click(function(){
+        $("#content3").fadeOut(300);
+        $("#content2").fadeOut(300, function() {
+            $("#content1").fadeIn(300, function(){
+
+                $('.card').on( "click", function() {
+                    switch ($(this).attr("id")){
+                        case "1" :
+                            $("#content3").load("includes/gals/chester.html");
+                            $("#content1").fadeOut(300, function() {
+                                $("#content3").fadeIn(300);
+                            });
+                            
+                        break;
+                        case "2":
+
+                    }
+                });
+
+                $(".card").on({
+                    mouseenter: function () {
+                        $(this).find(".card-img-top").css("opacity", "0.7")
+                    },
+                    mouseleave: function () {
+                        $(this).find(".card-img-top").css("opacity", "1")
+                    }
+                });
+            });
+        });
+        
+    });
+    
+    $('#askills').click(function(){
+        $(".workdiv").fadeOut(300, function() {
+            $("#content2").fadeIn(300);
+        });
+    });
+    
+    
+
+
  });
+
