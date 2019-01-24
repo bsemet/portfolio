4645235c7611462c54d839696e387e3c2594b6bc
Benoit Semet <benoitsemet1@gmail.com>
2019-01-24 16:56:18 +0100

    Correction pourcentages
    Correction pourcentages
    Ajout c4d
    nouveau fichier js

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,5 @@
 jQuery(document).ready(function ($) {
-    //$("#content").load("includes/galerie.html");
     $("#content").load("includes/work.html");
-    //$("#content2").load("includes/skills.html");
-
-
 
     $('#awork').click(function () {
         $("#awork").addClass("linkactive");
@@ -44,65 +40,7 @@ jQuery(document).ready(function ($) {
 
     });
 
-    $('#askills').click(function () {
-        $("#askills").addClass("linkactive");
-        $("#awork").removeClass("linkactive");
-
-        $("#content").fadeOut(300, function () {
-            $("#content").empty();
-            $("#content").load("includes/skills.html");
-            $("#content").fadeIn(300, function () {
-                $("#containerskills").find(".row").on({
-                    mouseenter: function () {
-
-                        $(this).children().addClass("hoverOn");
-                        $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': 2 * Math.PI * 35 }, 1);
-                        $('svg.radial-progress').each(function (index, value) {
-
-                            // Get percentage of progress
-                            percent = $(value).data('percentage');
-                            // Get radius of the svg's circle.complete
-                            radius = $(this).find($('circle.complete')).attr('r');
-                            // Get circumference (2πr)
-                            circumference = 2 * Math.PI * radius;
-                            // Get stroke-dashoffset value based on the percentage of the circumference
-                            strokeDashOffset = circumference - ((percent * circumference) / 100);
-                            // Transition progress for 1.25 seconds
-                            $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': strokeDashOffset }, 1250);
-
-                        });
-                    },
-                    mouseleave: function () {
-                        $(this).children().removeClass("hoverOn");
-                    }
-                });
-                $('svg.radial-progress').each(function (index, value) {
-                    $(this).find($('circle.complete')).removeAttr('style');
-                });
-
-                $('svg.radial-progress').each(function (index, value) {
-
-                    // Get percentage of progress
-                    percent = $(value).data('percentage');
-                    // Get radius of the svg's circle.complete
-                    radius = $(this).find($('circle.complete')).attr('r');
-                    // Get circumference (2πr)
-                    circumference = 2 * Math.PI * radius;
-                    // Get stroke-dashoffset value based on the percentage of the circumference
-                    strokeDashOffset = circumference - ((percent * circumference) / 100);
-                    // Transition progress for 1.25 seconds
-                    $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': strokeDashOffset }, 1250);
-
-                });
-            });
-        });
-
-    });
-
-
-
-    $("#awork").click();
-
+$("#awork").click();
 
 });
 
---
+++ js/skills.js
@@ -0,0 +1,70 @@
+jQuery(document).ready(function ($) {
+   
+    $('#askills').click(function () {
+        $("#askills").addClass("linkactive");
+        $("#awork").removeClass("linkactive");
+
+        $("#content").fadeOut(300, function () {
+            $("#content").empty();
+            $("#content").load("includes/skills.html");
+            $("#content").fadeIn(300, function () {
+                $("#containerskills").find(".row").on({
+                    mouseenter: function () {
+
+                        $(this).children().addClass("hoverOn");
+                        //Efface le remplissage
+                        $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': 2 * Math.PI * 35 }, 1);
+                        //Relance l'animation sur ce cercle
+                        animateCircle($(this).find($('svg.radial-progress')))
+                        
+                    },
+                    mouseleave: function () {
+                        $(this).children().removeClass("hoverOn");
+                    }
+                });
+                $('svg.radial-progress').each(function () {
+                    $(this).find($('circle.complete')).removeAttr('style');
+                });
+
+                //animateCircle($(".circle1"));
+                animateAllCircles();
+            });
+        });
+
+    });
+
+    //Anime un cercle, passé en parametre
+    function animateCircle(x) {
+        if (x) {
+            // Get percentage of progress
+            percent = $(x).attr("data-percentage")
+            // Get radius of the svg's circle.complete
+            radius = $(x).find($('circle.complete')).attr('r');
+            // Get circumference (2πr)
+            circumference = 2 * Math.PI * radius;
+            // Get stroke-dashoffset value based on the percentage of the circumference
+            strokeDashOffset = circumference - ((percent * circumference) / 100);
+            // Transition progress for 1.25 seconds
+            $(x).find($('circle.complete')).animate({ 'stroke-dashoffset': strokeDashOffset }, 1250);
+        }
+    }
+    //Anime tous les cercles
+    function animateAllCircles(){
+        $('svg.radial-progress').each(function (index, value) {
+
+            // Get percentage of progress
+            percent = $(value).data('percentage');
+            // Get radius of the svg's circle.complete
+            radius = $(this).find($('circle.complete')).attr('r');
+            // Get circumference (2πr)
+            circumference = 2 * Math.PI * radius;
+            // Get stroke-dashoffset value based on the percentage of the circumference
+            strokeDashOffset = circumference - ((percent * circumference) / 100);
+            // Transition progress for 1.25 seconds
+            $(this).find($('circle.complete')).animate({ 'stroke-dashoffset': strokeDashOffset }, 1250);
+
+        });
+    }
+
+});
+
