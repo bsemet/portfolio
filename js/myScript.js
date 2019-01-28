60d65220d1f6c63ebff55af73b490a3f7ed1cb6f
Benoit Semet <benoitsemet1@gmail.com>
2019-01-28 13:25:35 +0100

    Adding content

--- js/myScript.js
+++ js/myScript.js
@@ -21,7 +21,7 @@ jQuery(document).ready(function ($) {
                     $("#content").fadeOut(300, function () {
                         $("#content").empty();
                     });
-
+                    $('html,body').animate({ scrollTop: 0 }, 0);
                     switch ($(this).attr("id")) {
                         case "1":
                             $('#containerArticle').find('div').each(function () {
@@ -34,25 +34,57 @@ jQuery(document).ready(function ($) {
                                 + 'They wanted a logo that would symbolise the sharing between people. </br>'
                                 + 'The second banner was for the event "Clubbing Dead". Here the theme was Halloween. I used the play on words to make a banner'
                                 + ' that both relate to the clubbing theme and also the serie The Walking Dead.</div>');
-                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage1.jpg">')
-                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage3.jpg">')
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage1.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage3.jpg">');
                             $("#galleryTech").append('<i class="fas fa-tools"></i> Illustrator, Photoshop, Cinema4D');
+                            initGallery();
+                            break;
+                        case "2":
+                            $('#containerArticle').find('div').each(function () {
+                                $(this).empty();
+                            });
+                            $("#galleryTitle").append("Polyenco");
+                            $("#galleryText").append('<div class="card-body">During my studies at Polytech Lyon, '
+                                + "I have taken part of Polyenco for a year. Polyenco is a junior company, managed by students of the school. "
+                                + 'The objective is to earn some experience while being at school. I was a designer.</br>'
+                                + 'One of my missions was to create new business cards for the members.</div>');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polyenco/Recto.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polyenco/Verso.jpg">');
+                            $("#galleryTech").append('<i class="fas fa-tools"></i> GIMP');
                             initGallery();
                             break;
                         case "3":
                             $('#containerArticle').find('div').each(function () {
                                 $(this).empty();
                             });
-                            $("#galleryTitle").append("Pélerinage Sonore");
-                            $("#galleryText").append('<div class="card-body">Pélerinage Sonore is a techno festival organised by 3 DJ friends. '
-                                + 'They needed banners for the events on the social medias. </br>'
-                                + 'The first banner was for their second event. The event was called "Sharing Days". '
-                                + 'They wanted a logo that would symbolise the sharing between people. </br>'
-                                + 'The second banner was for the event "Clubbing Dead". Here the theme was Halloween. I used the play on words to make a banner'
-                                + ' that both relate to the clubbing theme and also the serie The Walking Dead.</div>');
-                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage1.jpg">')
-                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/pelerinage/pelerinage3.jpg">')
-                            $("#galleryTech").append('<i class="fas fa-tools"></i> Illustrator, Photoshop, Cinema4D');
+                            $("#galleryTitle").append("Polytech");
+                            $("#galleryText").append('<div class="card-body">During my studies at Polytech Lyon, '
+                                + "I've been in charge of the creation of banners that will represent the school for the class of 2017. </br>"
+                                + 'Every banner is different, and you can actually see the evolution of my skills through the years. </br>'
+                                + 'The last banner has been made for a friend that had a website linked to the school.</div>');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polytech/Polytech1a.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polytech/Polytech1b.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polytech/Polytech2.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polytech/Polytech3.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb" src="img/work/polytech/PolytechQuotes.jpg">');
+                            $("#galleryTech").append('<i class="fas fa-tools"></i> GIMP, Photoshop, Cinema4D');
+                            initGallery();
+                            break;
+                        case "4":
+                            $('#containerArticle').find('div').each(function () {
+                                $(this).empty();
+                            });
+                            $("#galleryTitle").append("Safe Bike");
+                            $("#galleryText").append('<div class="card-body">During my studies at Polytech Lyon, '
+                                + "for a team exercise, we had to create a fictionnal company, with a concept and a product to sell. </br>"
+                                + 'Our company was called Safe Bike and the product was an automatic break for kids bikes. '
+                                + 'If it detects that the bike is going too fast, it will safely stop the bike.</br>'
+                                + 'Part of my job was to create a logo for the company. Because we didnt have much time for this exercise, '
+                                + "I couldn't work in a Agile way, so I created three different logos and let my teammates choose their favorite. </div>");
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb safebike" src="img/work/safebike/safebike1.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb safebike" src="img/work/safebike/safebike2.jpg">');
+                            $("#gallerySlider").append('<img class="img-fluid img-thumbnail thumb safebike" src="img/work/safebike/safebike3.jpg">');
+                            $("#galleryTech").append('<i class="fas fa-tools"></i> Illustrator');
                             initGallery();
                             break;
                     }
