29d765470070f74d5d33ff101d4aefe555b29d20
Benoit Semet <benoitsemet1@gmail.com>
2019-01-30 21:33:40 +0100

    hover on profile row

--- js/profile.js
+++ js/profile.js
@@ -13,7 +13,14 @@ jQuery(document).ready(function ($) {
             $("#content").empty();
             $("#content").load("includes/profile.html");
             $("#content").fadeIn(300, function () {
-                
+                $(".rowprofile").find(".row").on({
+                    mouseenter: function () {
+                        $(this).children().addClass("hoverOn");
+                    },
+                    mouseleave: function () {
+                        $(this).children().removeClass("hoverOn");
+                    }
+                });
             });
         });
 
